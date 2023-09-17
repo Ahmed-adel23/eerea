0e44734aa65405c844eb54b7ddd410fcbd7d11b3
Ahmed-adel23 <121492522+Ahmed-adel23@users.noreply.github.com>
2023-09-17 14:27:19 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import NavBar from './NavBar';
+
+export default function App() {
+  return (
+    <View style={{flex:1 ,}}>
+      <NavBar/>
+    </View>
+  );
+}
